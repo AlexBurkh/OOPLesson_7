6e184c9d2493641430dcfb0b97cc08a72509db6a
alex.burkh@yandex.ru <alex.burkh@yandex.ru>
2022-04-05 19:09:05 +0300

    Задание 1 выполнено

---
+++ Task1/ACoder.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    public class ACoder : ICoder
+    {
+        public string Encode(string input)
+        {
+            string result = string.Empty;
+            for (int i = 0; i < input.Length; i++)
+            {
+                result += EncodeSymbol(input[i]);
+            }
+            return result;
+        }
+        public string Decode(string input)
+        {
+            string result = string.Empty;
+            for (int i = 0; i < input.Length; i++)
+            {
+                result += DecodeSymbol(input[i]);
+            }
+            return result;
+        }
+
+        private char EncodeSymbol(char symbol)
+        {
+            return ++symbol;
+        }
+        private char DecodeSymbol(char symbol)
+        {
+            return --symbol;
+        }
+    }
+}
